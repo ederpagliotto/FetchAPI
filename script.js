d7335a7038ea4dcdd50ce8e029e301368962ffd9
Eder Pagliotto <ederpagliotto@gmail.com>
2023-01-29 16:13:30 +0000

    adding cep according to number typed

---
+++ script.js
@@ -0,0 +1,21 @@
+const inputCep = document.getElementById('cep');
+const btnCep = document.getElementById('btnCep');
+const resultadoCep = document.querySelector('.resultadoCep');
+
+btnCep.addEventListener('click', handleClick);
+
+function handleClick(event) {
+  event.preventDefault();
+  const cep = inputCep.value;
+  buscaCep(cep);
+}
+
+function buscaCep(cep) {
+  fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    .then((response) => {
+      return response.text();
+    })
+    .then((body) => {
+      resultadoCep.innerText = body;
+    });
+}
